d632c1f1e4f75153b97568823b723739e4b78d08
davicaralh-tech <davicaralh@gmail.com>
2026-09-23 15:02:51 -0300

    Create page.js

---
+++ page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🇧🇷 EB ÔMEGA 🏴</h1>
+      <p>Escolha o seu lado:</p>
+
+      <button>🇧🇷 EXÉRCITO</button>
+      <button>🏴 CRIA</button>
+    </main>
+  );
+}
